1ab1703143831089d1ae3af614c7b9ac9518aac4
qbhfh <2682147541@qq.com>
2022-02-08 16:27:35 +0800

    fix: 恢复update_domain_ssl.js中的内容

--- scripts/update_domain_ssl.js
+++ scripts/update_domain_ssl.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const update_domain_ssl = require('scripts/update_domain_ssl.js');
+const qiniu = require('qiniu');
 require('dotenv').config({ path: '.env' });
 
 const {
@@ -11,11 +11,11 @@ const {
   JIANMU_HTTP2_ENABLE,
 } = process.env;
 
-const mac = new update_domain_ssl.auth.digest.Mac(JIANMU_ACCESS_KEY, JIANMU_SECRET_KEY);
+const mac = new qiniu.auth.digest.Mac(JIANMU_ACCESS_KEY, JIANMU_SECRET_KEY);
 // 获取token
-const domainToken = update_domain_ssl.util.generateAccessToken(
-  mac,
-  `https://api.qiniu.com/domain/${JIANMU_DOMAIN}`
+const domainToken = qiniu.util.generateAccessToken(
+    mac,
+    `https://api.qiniu.com/domain/${JIANMU_DOMAIN}`
 );
 
 const data = {
@@ -25,60 +25,60 @@ const data = {
 };
 
 axios
-  .get(`https://api.qiniu.com/domain/${JIANMU_DOMAIN}`, {
-    headers: {
-      Authorization: domainToken,
-    },
-  })
-  .then(res => {
-    console.log('status:', res.status);
-    console.log('statusText:', res.statusText);
-    console.log('data:', res.data);
-    if (res.data.protocol === 'http') {
-      const sslizeToken = update_domain_ssl.util.generateAccessToken(
-        mac,
-        `https://api.qiniu.com/domain/${JIANMU_DOMAIN}/sslize`
-      );
-      // http升级到https并修改证书
-      axios
-        .put(`https://api.qiniu.com/domain/${JIANMU_DOMAIN}/sslize`, data, {
-          headers: {
-            Authorization: sslizeToken,
-          },
-        })
-        .then(res => {
-          console.log('status:', res.status);
-          console.log('statusText:', res.statusText);
-          console.log('data:', res.data);
-        })
-        .catch(err => {
-          console.error(err);
-          process.exit(1);
-        });
-    } else {
-      const httpsConfToken = update_domain_ssl.util.generateAccessToken(
-        mac,
-        `https://api.qiniu.com/domain/${JIANMU_DOMAIN}/httpsconf`
-      );
-      // 已经是https直接修改证书
-      axios
-        .put(`https://api.qiniu.com/domain/${JIANMU_DOMAIN}/httpsconf`, data, {
-          headers: {
-            Authorization: httpsConfToken,
-          },
-        })
-        .then(res => {
-          console.log('status:', res.status);
-          console.log('statusText:', res.statusText);
-          console.log('data:', res.data);
-        })
-        .catch(err => {
-          console.error(err);
-          process.exit(1);
-        });
-    }
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+    .get(`https://api.qiniu.com/domain/${JIANMU_DOMAIN}`, {
+      headers: {
+        Authorization: domainToken,
+      },
+    })
+    .then(res => {
+      console.log('status:', res.status);
+      console.log('statusText:', res.statusText);
+      console.log('data:', res.data);
+      if (res.data.protocol === 'http') {
+        const sslizeToken = qiniu.util.generateAccessToken(
+            mac,
+            `https://api.qiniu.com/domain/${JIANMU_DOMAIN}/sslize`
+        );
+        // http升级到https并修改证书
+        axios
+            .put(`https://api.qiniu.com/domain/${JIANMU_DOMAIN}/sslize`, data, {
+              headers: {
+                Authorization: sslizeToken,
+              },
+            })
+            .then(res => {
+              console.log('status:', res.status);
+              console.log('statusText:', res.statusText);
+              console.log('data:', res.data);
+            })
+            .catch(err => {
+              console.error(err);
+              process.exit(1);
+            });
+      } else {
+        const httpsConfToken = qiniu.util.generateAccessToken(
+            mac,
+            `https://api.qiniu.com/domain/${JIANMU_DOMAIN}/httpsconf`
+        );
+        // 已经是https直接修改证书
+        axios
+            .put(`https://api.qiniu.com/domain/${JIANMU_DOMAIN}/httpsconf`, data, {
+              headers: {
+                Authorization: httpsConfToken,
+              },
+            })
+            .then(res => {
+              console.log('status:', res.status);
+              console.log('statusText:', res.statusText);
+              console.log('data:', res.data);
+            })
+            .catch(err => {
+              console.error(err);
+              process.exit(1);
+            });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      process.exit(1);
+    });
